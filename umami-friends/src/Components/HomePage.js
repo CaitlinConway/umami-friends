3cd2cd560201bee270106e20dd25fcbdecd1293b
Caitlin Conway <coolcac@gmail.com>
2024-01-04 16:03:15 -0600

    add randomizing new rooms

--- umami-friends/src/Components/HomePage.js
+++ umami-friends/src/Components/HomePage.js
@@ -5,11 +5,24 @@ import useUserInfo from '../Hooks/useUserInfo'
 
 const socket = io('http://localhost:3030');
 
+const generateRandomRoom = () => {
+    //uppercase letters and numbers so make sure to force into uppercase when joining same room
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomRoomCode = '';
+
+    for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomRoomCode += characters.charAt(randomIndex);
+    }
+
+    return randomRoomCode;
+};
 
 function HomePage() {
     //make this hook
     const { userName, setUserName, setRoomCode, roomCode } = useUserInfo();
     const [userInput, setUserInput] = useState('');
+    const [roomInput, setRoomInput] = useState('')
 
     const sendUser = () => {
         console.log(`Setting user: ${userInput}`);
@@ -24,13 +37,29 @@ function HomePage() {
         // Clear the message input field
         setUserInput('');
     };
+    const enterNewRoom = () => {
+        const newRoomCode = generateRandomRoom()
+        console.log(`Entering new`)
+        socket.emit('joinNewRoom', newRoomCode)
+        setRoomCode(newRoomCode)
+        setRoomInput('')
+    }
+    const enterExistingRoom = () => {
+        if (roomInput != "") {
+            roomInput = roomInput.toUpperCase()
+        }
+        console.log(`Entering room: ${roomInput}`)
+        socket.emit('joinRoom', roomInput)
+        setRoomCode(roomInput)
+        setRoomInput('')
+    }
 
     //TODO: Add slice of state for room code and another input box. Second button for start new?
     return (
         <div>
             Umami Friends
             {userName ? <div className='userName'>User: {userName}</div> : <></>}
-            <div className='userInput'>
+            {!userName ? (<div className='userInput'>
                 <input
                     type="text"
                     value={userInput}
@@ -38,10 +67,12 @@ function HomePage() {
                     onKeyDown={(e) => e.key == 'Enter' ? sendUser() : null}
                 />
                 <button onClick={sendUser}>Set User Name</button>
+            </div>) : null}
+            <div className='roomInput'>
                 <button onClick={enterNewRoom}>Enter new room</button>
                 <input
                     type="text"
-                    value={userInput}
+                    value={roomInput}
                     onChange={(e) => setRoomCode(e.target.value)}
                     onKeyDown={(e) => e.key == 'Enter' ? enterExistingRoom() : null}
                 />
