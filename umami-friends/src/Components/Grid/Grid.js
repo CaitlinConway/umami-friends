a51fdc1e6bc164103ae78c6be3dd3290bef04317
Caitlin Conway <coolcac@gmail.com>
2024-01-11 00:29:36 -0600

    add pile counts

--- umami-friends/src/Components/Grid/Grid.js
+++ umami-friends/src/Components/Grid/Grid.js
@@ -10,6 +10,7 @@ export const Grid = (props) => {
         <div className="grid-container">
             {Object.keys(basicRecipes).map((recipe, index) => {
                 const img = require(`../../Pictures/${recipe}.png`)
+                const pileCount = gameState.basicRecipes[recipe]
                 return (
                     <div className='grid-item' key={index}>
                         <img data-tooltip-id={`imageTooltip${index}`} className='recipeCard' alt={`${recipe}`} src={img} />
@@ -21,6 +22,7 @@ export const Grid = (props) => {
                             opacity={1}
                         >
                             <img className='hoveredImage' alt={`${recipe}`} src={img} />
+                            <div className='tooltip-text'>{`Pile Count: ${pileCount}`}</div>
                         </Tooltip>
                     </div>
                 )
