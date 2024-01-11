ad7ef3e3a76827d6bbecfe7b63801863e695b6a6
Caitlin Conway <coolcac@gmail.com>
2024-01-10 23:01:09 -0600

    header styling added

--- umami-friends/src/Components/Header/Header.js
+++ umami-friends/src/Components/Header/Header.js
@@ -3,15 +3,17 @@ import './Header.css'
 export const Header = (props) => {
     return (
         <div className="header">
-            <div className="header-text">
+            <div className='header-middle'>
+                <div className='header-userName'>User: {props.userName}</div>
+                <div className='header-roomCode'>Room Code: {props.roomCode}</div>
+            </div>
+            <div className="header-title-container">
                 <div className="header-title">Umami Friends</div>
                 <div className="header-subtitle">A game of culinary creation</div>
-                <div className='header-bottom-row'>
-                    <div className='header-subtitle'>User: {props.userName}</div>
-                    <div className='header-subtitle'>Room Code: {props.roomCode}</div>
-                    <button className='header-subtitle' onClick={props.startGame}>Start Game</button>
+            </div>
+            <div className='header-right'>
+                <button className='start-button' onClick={props.startGame}>Start Game</button>
 
-                </div>
             </div>
         </div>
     );
