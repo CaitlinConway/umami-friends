608a204a02873648142562e7e39acdc0b7d82a69
Caitlin Conway <coolcac@gmail.com>
2024-03-09 08:51:06 -0600

    add new pics

--- umami-friends/src/Components/RecipeCard/RecipeCard.js
+++ umami-friends/src/Components/RecipeCard/RecipeCard.js
@@ -1,27 +1,32 @@
 import React from "react";
-import './RecipeCard.css'
+import "./RecipeCard.css";
 // Sample Card with props
 // Future improvement could make dynamic so ez change rules instead of static whole card pics
 
-// name: "Spring Roll",
-// pictureName: "springRoll",
-// points: 2,
-// status: "basicPlant",
-// cost: {
-//     redTriangle: 1,
-//     greenCircle: 3
-// },
-// energy: 1,
-// description: "Place 2 ingredients from your hand onto your board. Refresh. All other players draw +1 card",
-// actions: ["energy1", "placeIngredients2", "otherPlayersDraw"]
-
+// {
+//     "name": "Healthy Snack",
+//     "pictureName": "healthySnack",
+//     "points": 1,
+//     "status": "basicPlant",
+//     "cost": {
+//       "ingredientPlant": 2
+//     },
+//     "energy": 1,
+//     "description": "All other players draw +1 card.",
+//     "actions": ["Snack"],
+//     "pileCount": 6
+//   },
 
 export const RecipeCard = (props) => {
-    return (
-        <div className="card">
-            <div className="card-body">
-                <img className='recipeCard' alt={`${props.name}`} src={require(`../Pictures/${props.pictureName}.png`)} />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="card">
+      <div className="card-body">
+        <img
+          className="recipeCard"
+          alt={`${props.name}`}
+          src={require(`../Pictures/${props.pictureName}.png`)}
+        />
+      </div>
+    </div>
+  );
+};
