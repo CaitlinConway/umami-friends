60f4fce288dae7dceb2961f0b88c2fa606baf83e
Caitlin Conway <coolcac@gmail.com>
2024-03-24 14:54:27 -0500

    fix move logic so it modifies original gamestate passed into function, add some todos for fixing ingredient piles

--- server/Helpers/gameActionHelper.js
+++ server/Helpers/gameActionHelper.js
@@ -18,15 +18,19 @@ const allCards = {
 const moveCard = ({ fromStack, toStack, cards }) => {
   toStack?.push(...cards.map((each) => allCards[each]));
   let cardsToRemove = [...cards];
-  fromStack = fromStack.filter((each) => {
-    if (cardsToRemove.includes(each.pictureName)) {
-      cardsToRemove = cardsToRemove.filter(
-        (cardName) => cardName === each.pictureName
-      );
-      return false;
-    }
-    return true;
-  });
+  let stack = fromStack.splice(0, fromStack.length); // Clear the fromStack array
+
+  fromStack.push(
+    ...stack.filter((each) => {
+      if (cardsToRemove.includes(each.pictureName)) {
+        cardsToRemove = cardsToRemove.filter(
+          (cardName) => cardName === each.pictureName
+        );
+        return false;
+      }
+      return true;
+    })
+  );
 };
 
 const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
@@ -46,6 +50,7 @@ export function gameActionHelper(socket, io, action, roomCode, userName) {
       break;
     case "startGame":
       gameState.playerTurn = 1;
+      //TODO: fix this so it draws from gamestate ingredient draw pile
       gameState.users.forEach((user) => {
         user.hand = shuffleCards(ingredientsCopy, 5);
       });
@@ -73,6 +78,7 @@ export function gameActionHelper(socket, io, action, roomCode, userName) {
       currentPlayer.energy = 1;
       break;
     case "drawCard":
+      //TODO fix this so that it pulls from the gamestate draw pile
       currentPlayer.hand.push(...shuffleCards(ingredientsCopy, 3));
       currentPlayer.energy--;
       break;
